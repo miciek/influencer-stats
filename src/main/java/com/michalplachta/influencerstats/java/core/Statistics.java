22256c5d3c17c9c5715207173348549271fcd125
miciek <miciek@gmail.com>
2019-04-06 11:16:16 +0300

    Fix the Java version to be semantically the same as Scala one

--- src/main/java/com/michalplachta/influencerstats/java/core/Statistics.java
+++ src/main/java/com/michalplachta/influencerstats/java/core/Statistics.java
@@ -31,45 +31,50 @@ public class Statistics {
                 .thenCompose(ignored ->
                         collectionView.fetchCollection(collectionId)
                 )
-                .thenCompose(collection -> {
-                    logging.info("fetched collection: " + collection);
-                    List<String> videoIds = collection.map(c -> c.videos).orElse(emptyList());
-                    logging.info("going to make " + videoIds.size() + " fetches");
-                    return sequence(videoIds
-                            .stream()
-                            .map(videoClient::fetchVideoListResponse).collect(toList()));
-                })
-                .thenApply(responses -> {
-                    logging.info("got responses: " + responses);
-                    List<InfluencerItem> items = responses.stream().flatMap(r -> responseToItems(r).stream()).collect(toList());
-                    logging.info("got list of influencer items: " + items);
-                    return calculate(items);
-                });
-    }
-
-    private static CollectionStats calculate(List<InfluencerItem> items) {
-        return items.stream().reduce(new CollectionStats(0, 0, 0), (results, item) ->
-                        new CollectionStats(
-                                results.impressions + item.views,
-                                results.engagements + item.comments + item.likes + item.dislikes,
-                                results.engagements + item.likes
-                        ), (a, b) -> new CollectionStats(a.impressions + b.impressions, a.engagements + b.engagements, a.score + b.score)
+                .thenCompose(collection ->
+                    logging.info("fetched collection: " + collection)
+                            .thenCompose(ignored -> {
+                                List<String> videoIds = collection.map(c -> c.videos).orElse(emptyList());
+                                return logging.info("going to make " + videoIds.size() + " fetches")
+                                        .thenCompose(ignored_ ->
+                                            sequence(videoIds
+                                                .stream()
+                                                .map(videoClient::fetchVideoListResponse).collect(toList())));
+                            })
+                )
+                .thenCompose(responses ->
+                    logging.info("got responses: " + responses)
+                        .thenCompose(ignored -> {
+                            List<InfluencerItem> items = responses.stream().flatMap(r -> responseToItems(r).stream()).collect(toList());
+                            return logging.info("got list of influencer items: " + items)
+                                    .thenApply(ignored_ -> calculate(items));
+                        })
                 );
     }
 
-    private static List<InfluencerItem> responseToItems(VideoListResponse response) {
-        return response.items
-                .stream()
-                .map(v -> v.statistics)
-                .map(video -> new InfluencerItem(video.viewCount, video.likeCount, 0, 0))
-                .collect(Collectors.toList());
-    }
-
-    private static <T> CompletableFuture<List<T>> sequence(List<CompletableFuture<T>> futures) {
+    public static <T> CompletableFuture<List<T>> sequence(List<CompletableFuture<T>> futures) {
         return futures.stream()
                 .map(f -> f.thenApply(Stream::of))
                 .reduce((a, b) -> a.thenCompose(xs -> b.thenApply(ys -> concat(xs, ys))))
                 .map(f -> f.thenApply(s -> s.collect(toList())))
                 .orElse(completedFuture(emptyList()));
+    }
+
+    public static CollectionStats calculate(List<InfluencerItem> items) {
+        return items.stream().reduce(new CollectionStats(0, 0, 0), (results, item) ->
+                new CollectionStats(
+                        results.impressions + item.views,
+                        results.engagements + item.comments + item.likes + item.dislikes,
+                        results.engagements + item.likes
+                ), (a, b) -> new CollectionStats(a.impressions + b.impressions, a.engagements + b.engagements, a.score + b.score)
+        );
+    }
+
+    public static List<InfluencerItem> responseToItems(VideoListResponse response) {
+        return response.items
+                .stream()
+                .map(v -> v.statistics)
+                .map(video -> new InfluencerItem(video.viewCount, video.likeCount, 0, 0))
+                .collect(Collectors.toList());
     }
 }
